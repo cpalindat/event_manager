85cfbff4fd1e6fbc0f92a958179c00ec2c5f09fc
Connor <connorpalindat@gmail.com>
2024-08-01 13:09:47 -0600

    Created method for determining peak hours of registration.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -45,6 +46,30 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def peak_registration_hours(csv)
+  # Create hash to store occurences
+  occurences = Hash.new(0)
+
+  # Loop over CSV file and count occuerneces of the hours of registration
+  csv.each do |row|
+    hour = Time.strptime(row[:regdate], '%m/%d/%y %H:%M').hour
+    occurences[hour] += 1
+  end
+
+  # Calculate the max occurence
+  max_occurence = occurences.values.max
+  puts "Peak Hours for Registration:"
+  occurences.each do |value|
+    if value[1] == max_occurence
+      puts value[0]
+    end
+  end
+end
+
+def peak_registration_day(csv)
+  
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -52,6 +77,8 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
 )
+
+peak_registration_hours(contents)
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
